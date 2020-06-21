723d9e1d1a1e6cc337a13f39528f2ad9ff8f6cf9
kooku <koomg9599@gmail.com>
2020-06-21 15:15:35 +0900

    feat: Create cart reducer

---
+++ src/stores/modules/cart/actions.ts
@@ -0,0 +1,12 @@
+import { ADD_CART, DELETE_CART } from "./types";
+import { TProduct } from "~stores";
+
+export const addCart = (product: TProduct) => ({
+  type: ADD_CART,
+  payload: product,
+});
+
+export const deleteCart = (id: number) => ({
+  type: DELETE_CART,
+  payload: id,
+});
---
+++ src/stores/modules/cart/reducers.ts
@@ -0,0 +1,29 @@
+import { ADD_CART, DELETE_CART, CartState } from "./types";
+import { TProduct } from "~stores";
+
+const initialState: CartState = {
+  products: JSON.parse(window.localStorage.getItem("cart") ?? []),
+};
+
+export const cart = (state: CartState = initialState, action: any) => {
+  switch (action.type) {
+    case ADD_CART:
+      // eslint-disable-next-line no-case-declarations
+      const addedProducts = state.products.concat(action.payload);
+      window.localStorage.setItem("cart", JSON.stringify(addedProducts));
+      return {
+        products: addedProducts,
+      };
+    case DELETE_CART:
+      // eslint-disable-next-line no-case-declarations
+      const deletedProducts = state.products.filter(
+        (product: TProduct) => product.id !== action.payload
+      );
+      window.localStorage.setItem("cart", JSON.stringify(deletedProducts));
+      return {
+        products: deletedProducts,
+      };
+    default:
+      break;
+  }
+};
