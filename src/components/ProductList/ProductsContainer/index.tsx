f7efaf79d1ff3e40ad2e66a8016d50eadac2cf7d
kooku <koomg9599@gmail.com>
2020-06-21 13:38:05 +0900

    feat: Create pagination common component

--- src/components/ProductList/ProductsContainer/index.tsx
+++ src/components/ProductList/ProductsContainer/index.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { TProduct } from "~stores";
 import Product from "~components/ProductList/ProductsContainer/Product";
+import Pagination from "~components/Common/Pagination";
+
+import "./style.scss";
 
 function ProductsContainer() {
   const { products } = useSelector((state) => state.products);
+  const sortedProducts = useMemo(
+    () => products.sort((a: TProduct, b: TProduct) => b.score - a.score),
+    [products]
+  );
   return (
-    <ul className="list-products row">
-      {products.map((product: TProduct) => (
-        <li
-          className="list-products-item col-12 col-md-4 col-lg-3"
-          key={product.id}
-        >
-          <Product {...product} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list-products row">
+        {sortedProducts.map((product: TProduct) => (
+          <li
+            className="list-products-item col-12 col-md-4 col-lg-3"
+            key={product.id}
+          >
+            <Product {...product} />
+          </li>
+        ))}
+      </ul>
+      <div className="mx-auto">
+        <Pagination />
+      </div>
+    </>
   );
 }
 
